311d1a6aa8e8932a65523f802f13bb5b32c1d88e
sosiki1997 <3599186771@qq.com>
2022-05-09 12:50:15 +0800

    interface

--- src/main.js
+++ src/main.js
@@ -1,6 +1,41 @@
 import Vue from "vue";
 import router from "./router";
+import axios from "axios";
+// vue-axios把作用域对象挂载到vue实例上去，方便用this去调用
+import VueAxios from "vue-axios";
+// 插件放上面，组件放下面
 import App from "./App.vue";
+import env from "./env";
+
+/* 接口规范
+{
+  status:0, //状态码
+  data:[],  //数据
+  msg:''    //报错
+}
+*/
+
+// 根据前端的跨域方式做调整.把环境变量抽取到一个模块里去，集中进行管理
+axios.defaults.baseURL = "/api"; //代理跨域
+axios.defaults.timeout = 8000;
+// 根据环境变量获取不同的请求地址
+axios.defaults.baseURL = env.baseURL;
+// 接口错误拦截
+axios.interceptors.response.use(function (response) {
+  let res = response.data; //取到接口返回的值
+  // 成功
+  if (res.satus == 0) {
+    return res.data;
+    // 登录拦截
+  } else if (res.status == 10) {
+    window.location.href = "/#/login";
+    // 弹出错误信息
+  } else {
+    alert(res.msg);
+  }
+});
+
+Vue.use(VueAxios, axios);
 
 Vue.config.productionTip = false;
 
