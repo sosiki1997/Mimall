fa3f4822f282b3b05e4ee539b377f9083339065e
sosiki1997 <3599186771@qq.com>
2022-05-11 16:35:31 +0800

    swiperSlide

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,18 +1,99 @@
 <!-- 首页 -->
 
 <template>
-  <service-bar></service-bar>
+  <div class="index">
+    <!-- 安全区域 -->
+    <div class="container">
+      <div class="swiper-box">
+        <swiper v-bind:options="swiperOption">
+          <swiper-slide v-for="(item, index) in slideList" v-bind:key="index">
+            <a v-bind:href="'/#/product/' + item.id"
+              ><img v-bind:src="item.img"
+            /></a>
+          </swiper-slide>
+          <div class="swiper-pagination" slot="pagination"></div>
+          <div class="swiper-button-prev" slot="button-prev"></div>
+          <div class="swiper-button-next" slot="button-next"></div>
+        </swiper>
+      </div>
+      <div class="ads"></div>
+      <div class="banner"></div>
+      <div class="product-box"></div>
+    </div>
+    <service-bar></service-bar>
+  </div>
 </template>
 
 <script>
 import ServiceBar from "./../components/ServiceBar";
+import { swiper, swiperSlide } from "vue-awesome-swiper";
 
+import "swiper/dist/css/swiper.css";
 export default {
   name: "index",
   components: {
+    swiper,
+    swiperSlide,
     ServiceBar,
+  },
+  data() {
+    return {
+      swiperOption: {
+        autoplay: true,
+        loop: true,
+        effect: "fade",
+        fadeEffect: {
+          crossFade: true,
+        },
+        // 分页器 小圆点
+        pagination: {
+          el: ".swiper-pagination",
+          // 点击切换
+          clickable: true,
+        },
+        // 导航 左右箭头
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      },
+      slideList: [
+        {
+          id: "42",
+          img: "/imgs/slider/slide-1.jpg",
+        },
+        {
+          id: "45",
+          img: "/imgs/slider/slide-2.jpg",
+        },
+        {
+          id: "46",
+          img: "/imgs/slider/slide-3.jpg",
+        },
+        {
+          id: "",
+          img: "/imgs/slider/slide-4.jpg",
+        },
+        {
+          id: "",
+          img: "/imgs/slider/slide-1.jpg",
+        },
+      ],
+    };
   },
 };
 </script>
 
-<style></style>
+<style lang="scss">
+.index {
+  .swiper-box {
+    .swiper-container {
+      height: 451px;
+      img {
+        width: 100%;
+        height: 100%;
+      }
+    }
+  }
+}
+</style>
